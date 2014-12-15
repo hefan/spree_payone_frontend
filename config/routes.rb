e7cbaa858bc4682775438cae8ef6218ea467e680
Stefan Hartmann <stefan@pfeffans.local>
2014-12-15 16:35:48 +0100

    more precise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
 
-  match '/payone_frontend/status',  :to => 'payone_frontend#status'
-  match '/payone_frontend/success', :to => 'payone_frontend#success'
-  match '/payone_frontend/cancel',  :to => 'payone_frontend#cancel'
+  get '/payone_frontend/status',  :to => 'payone_frontend#status'
+  get '/payone_frontend/success', :to => 'payone_frontend#success'
+  match '/payone_frontend/cancel',  :to => 'payone_frontend#cancel', via: [:get, :post]
 
 end
