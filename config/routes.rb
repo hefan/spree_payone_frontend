3f90023b12d63e72ac5ac26b731f39113689bb80
stefan <stefan@yo-code.de>
2014-12-15 03:13:19 +0100

    Fix routes to match POST requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
 
-  get '/payone_frontend/status', :to => 'payone_frontend#status'
-  get '/payone_frontend/success', :to => 'payone_frontend#success'
-  get '/payone_frontend/cancel', :to => 'payone_frontend#cancel'
+  match '/payone_frontend/status',  :to => 'payone_frontend#status'
+  match '/payone_frontend/success', :to => 'payone_frontend#success'
+  match '/payone_frontend/cancel',  :to => 'payone_frontend#cancel'
 
 end
