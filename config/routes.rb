92f3e32e804b432bdc11876e7b610fa319f4e5a7
stefan <stefan@yo-code.de>
2014-12-16 00:56:15 +0100

    payone cancel and sucess requests are get, payone status requests are post or get, Resolved #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Spree::Core::Engine.routes.draw do
 
-  get '/payone_frontend/status',  :to => 'payone_frontend#status'
+  match '/payone_frontend/status',  :to => 'payone_frontend#status', via: [:get, :post]
   get '/payone_frontend/success', :to => 'payone_frontend#success'
-  match '/payone_frontend/cancel',  :to => 'payone_frontend#cancel', via: [:get, :post]
+  get '/payone_frontend/cancel',  :to => 'payone_frontend#cancel'
 
 end
