5a9889c5949e49f2f1e395895f3b3b4cae9de394
Stefan Hartmann <stefan@wemove.com>
2015-01-02 03:21:50 +0100

    use scope resolution operator

--- lib/spree/payone_frontend/status_check.rb
+++ lib/spree/payone_frontend/status_check.rb
@@ -21,8 +21,8 @@ module Spree::PayoneFrontend
         '185.60.20.0/24'
       ].any? do |address_block|
         begin
-          IPAddr.new(address_block) === @request.remote_ip
-        rescue IPAddr::InvalidAddressError => iae
+          ::IPAddr.new(address_block) === @request.remote_ip
+        rescue ::IPAddr::InvalidAddressError => iae
           return false
         end
       end
